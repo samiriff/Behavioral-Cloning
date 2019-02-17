9d735ab212a029bcbe26001021689e59b193e970
I302861 <samir.sheriff@sap.com>
2019-02-17 16:50:40 +0530

    Adding utilities to transfer files from my laptop to the workspace

---
+++ data_extractor.py
@@ -0,0 +1,26 @@
+import zipfile
+import os
+from tqdm import tqdm
+import sys
+
+class DataExtractor:
+    def __init__(self, src, dst):
+        self.src = src
+        self.dst = dst
+
+    def unzip_data(self):
+        zip = zipfile.ZipFile(self.src, 'r')
+        nameList = zip.namelist()
+
+        count = 0
+
+        for item in tqdm(nameList, desc='Unzipping'):
+            dir, file = os.path.split(item)
+            if dir.find("__MACOSX") == -1:
+                # print(count, item, "Dir=", dir, "File=", file)
+                count += 1
+                zip.extract(item, self.dst)
+        zip.close()
+if __name__ == '__main__':
+    data_extractor = DataExtractor(sys.argv[1], sys.argv[2])
+    data_extractor.unzip_data()
