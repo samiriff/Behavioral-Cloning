fe81288d63d3f363910f376cae4a41dcacf346eb
I302861 <samir.sheriff@sap.com>
2019-02-18 14:26:18 +0530

    Added support for data pruning

--- data_server.py
+++ data_server.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import shutil
 import os
 import glob
@@ -28,6 +29,36 @@ class DataProvider:
         self.driving_log['left'] = self.driving_log['left'].apply(lambda path: '/'.join(path.split('\\')[-2:]))
         self.driving_log['right'] = self.driving_log['right'].apply(lambda path: '/'.join(path.split('\\')[-2:]))
 
+    def prune(self, num_to_remove):
+        print("Pruning")
+        pruned_df, dropped_indices = self.get_pruned_df(num_to_remove)
+        print("Dropped Indices Len = ", len(dropped_indices))
+        print("Original Data Frame Len = ", len(self.driving_log))
+        print("Pruned Data Frame Len = ", len(pruned_df))
+        self.delete_dropped_images(self.driving_log.iloc[dropped_indices])
+        self.driving_log = pruned_df
+
+    def get_pruned_df(self, num_to_remove):
+        drop_indices = np.random.choice(self.driving_log.index[self.driving_log['steering'] == 0], num_to_remove, replace=False)
+        return self.driving_log.drop(drop_indices), drop_indices
+
+    def delete_dropped_images(self, dropped_rows):
+        print("Deleting images after pruning")
+        dropped_rows['center'].apply(
+            lambda img: self.delete_image(img))
+        dropped_rows['left'].apply(
+            lambda img: self.delete_image(img))
+        dropped_rows['right'].apply(
+            lambda img: self.delete_image(img))
+
+    def delete_image(self, filename):
+        path = self.data_root + '/' + filename
+        if os.path.exists(path):
+            os.remove(path)
+            return True
+        print('Not found: ', path)
+        return False
+
     def write_driving_log(self):
         print("Writing CSV")
         self.driving_log.to_csv(self.data_root + '/driving_log.csv', index=False)
@@ -43,11 +74,17 @@ def startNgrok():
 
 
 if __name__ == '__main__':
-    data_provider = DataProvider('../new_data')
-    data_provider.process_driving_log()
-    data_provider.write_driving_log()
-    data_provider.create_zip()
-    #startNgrok()
+    data_root = '../turn_correction_data'
+    prune_num_rows = 200
+    if os.path.exists(data_root + ' - Copy'):
+        data_provider = DataProvider(data_root)
+        data_provider.process_driving_log()
+        data_provider.prune(prune_num_rows)
+        data_provider.write_driving_log()
+        data_provider.create_zip()
+        #startNgrok()
+    else:
+        print("IMPORTANT: Please make a backup of this folder before running this program")
 
 
 
